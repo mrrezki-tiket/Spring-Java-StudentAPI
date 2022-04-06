06a82f979570171bcb0711f0d47d8fa35fdb2f5e
Muhammad Rezki Ananda <muhammad.ananda@tiket.com>
2022-04-06 16:56:50 +0700

    Query From Method Name

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -40,22 +40,34 @@ public class DemoApplication {
 
 			);
 
-			Query query = new Query();
-			query.addCriteria(Criteria.where("email").is(email));
+			//usingMongoTemplateAndQuery(repository, mongoTemplate, email, student);
 
-			List<Student> students = mongoTemplate.find(query, Student.class);
-
-			if (students.size() > 1) {
-				throw new IllegalStateException("Found many student with email " + email);
-			}
-
-			if (students.isEmpty()) {
-				System.out.println("Inserting student " + student);
-				repository.insert(student);
-			} else {
-				System.out.println(student + " already exists");
-			}
+			repository.findStudentByEmail(email)
+					.ifPresentOrElse(s -> {
+						System.out.println(student + " already exists");
+					}, () -> {
+						System.out.println("Inserting student " + student);
+						repository.insert(student);
+					});
 		};
+	}
+
+	private void usingMongoTemplateAndQuery(StudentRepository repository, MongoTemplate mongoTemplate, String email, Student student) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("email").is(email));
+
+		List<Student> students = mongoTemplate.find(query, Student.class);
+
+		if (students.size() > 1) {
+			throw new IllegalStateException("Found many student with email " + email);
+		}
+
+		if (students.isEmpty()) {
+			System.out.println("Inserting student " + student);
+			repository.insert(student);
+		} else {
+			System.out.println(student + " already exists");
+		}
 	}
 
 }
